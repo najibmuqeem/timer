d4006cb794a5eef14db770f7c6815e710b106fb2
Najib <najib.muqeem@gmail.com>
2020-02-11 18:04:00 +0000

    minor timer fix

--- timer1.js
+++ timer1.js
@@ -1,8 +1,10 @@
 const args = process.argv;
-for (let i in args) {
-  if (!(isNaN(args[i]) && args[i] < 0)) {
-    setTimeout(() => {
-      process.stdout.write("\x07");
-    }, 1000 * args[i]);
+if (args[0]) {
+  for (let i in args) {
+    if (!(isNaN(args[i]) && args[i] < 0)) {
+      setTimeout(() => {
+        process.stdout.write("\x07");
+      }, 1000 * args[i]);
+    }
   }
 }
