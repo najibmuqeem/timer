47e37600489948e99aa082489f331056c5d870e7
Najib <najib.muqeem@gmail.com>
2020-02-11 18:22:51 +0000

    created another timer that utilizes user input

---
+++ timer2.js
@@ -0,0 +1,19 @@
+const stdin = process.stdin;
+
+stdin.setRawMode(true);
+stdin.setEncoding("utf8");
+
+stdin.on("data", key => {
+  if (key === "\u0003") {
+    process.stdout.write("Thanks for using me, ciao!\n");
+    process.exit();
+  }
+  if (key === "b") {
+    process.stdout.write("\x07");
+  }
+  if (key >= 1 && key <= 9) {
+    process.stdout.write(`setting timer for ${key} seconds...\n`);
+    setTimeout(() => process.stdout.write("\x07"), 1000 * key);
+  }
+  // process.stdout.write('.');
+});
